907db6f7972deef606aa1e2ec7a998e522893649
Coder-dev-sys <dvvyas652@gmail.com>
2025-05-02 11:15:45 +0530

    Worked on navbar

---
+++ Components/script.js
@@ -0,0 +1,20 @@
+// let menu=document.querySelector('#mnu-icon');
+// let navbar=document.querySelector('.navbar');
+    
+// if(menu!=null){
+//     menu.addEventListener('click',() => 
+//         {
+//                 menu.classList.toggle('bx-x');
+//                 navbar.classList.toggle('active');
+//         });
+// }
+// else
+// {
+//     console.log('null');
+// }
+
+// window.addEventListener('scroll',() =>
+// {
+//     menu.classList.remove('bx-x');
+//     navbar.classList.remove('active');
+// });
