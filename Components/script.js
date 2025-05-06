5fd73527c17dbdcde8bf29646b670dbbdb33cae1
Coder-dev-sys <dvvyas652@gmail.com>
2025-05-06 13:54:33 +0530

    Completed Home Section

--- Components/script.js
+++ Components/script.js
@@ -1,6 +1,6 @@
 /**  Navbar Section  **/
 
-/* Toggle menu on mobile devices */
+// Toggle menu on mobile devices 
 const menuCheckbox = document.getElementById("check");
 const navbar = document.querySelector(".navbar");
 if (menuCheckbox) {
@@ -31,3 +31,27 @@ window.addEventListener("scroll", () => {
 
 
 /**  Home Section  **/
+
+// Set minimum date to today for date pickers
+const today = new Date().toISOString().split('T')[0];
+document.getElementById('pickup-date').setAttribute('min', today);
+document.getElementById('dropoff-date').setAttribute('min', today);
+
+// When pickup date changes, update minimum dropoff date
+document.getElementById('pickup-date').addEventListener('change', function () {
+  document.getElementById('dropoff-date').setAttribute('min', this.value);
+
+  // If dropoff date is before new pickup date, update it
+  if (document.getElementById('dropoff-date').value < this.value) {
+    document.getElementById('dropoff-date').value = this.value;
+  }
+});
+
+
+// clickable Search icon
+const searchForm = document.querySelector('.search-form');
+const searchIcon = document.querySelector('.search-icon');
+
+searchIcon.addEventListener('click', () => {
+  searchForm.submit();
+});
